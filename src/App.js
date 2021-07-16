c919b0b420c5673451ad011bb383ef3dec435f4b
Shribatsa01 <shribatsa01@gmail.com>
2021-07-16 23:22:15 +0530

    added navbar and created page components.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,30 @@
+// import reportWebVitals from './reportWebVitals';
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import Navs from './components/Navs';
+import Home from './pages/Home';
+import { Starred } from './pages/Starred';
 
 function App() {
   return (
-    <Switch>
-      <Route exact path="/">
-        This is homepage
-      </Route>
+    <div>
+      <Navs />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
 
-      <Route exact path="/starred">
-        This is starred
-      </Route>
+        <Route exact path="/starred">
+          <Starred />
+        </Route>
 
-      <Route>This is 404 page</Route>
-    </Switch>
+        <Route>
+          <div>Not Found</div>
+        </Route>
+      </Switch>
+    </div>
   );
 }
 
 export default App;
+// reportWebVitals();
---
+++ src/pages/Starred.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const Starred = () => {
+  return <div>This is starred</div>;
+};
