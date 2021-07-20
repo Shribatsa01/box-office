71e8f4113c2ed49e353e814911e6b9f6499a0d2f
Shribatsa01 <shribatsa01@gmail.com>
2021-07-20 20:44:08 +0530

    Displayed show results and small fetch refactor.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,14 @@
 import React, { useState } from 'react';
 import MainPageLayout from '../components/MainPageLayout';
+import { apiGet } from '../misc/config';
 
 const Home = () => {
   const [input, setInput] = useState('');
+  const [results, setResults] = useState(null);
   const onSearch = () => {
-    // https://api.tvmaze.com/search/shows?q=girls
-
-    fetch(`https://api.tvmaze.com/search/shows?q=${input}`)
-      .then(r => r.json())
-      .then(result => {
-        console.log(result);
-      });
+    apiGet(`/search/shows?q=${input}`).then(result => {
+      setResults(result);
+    });
   };
 
   const onInputChange = ev => {
@@ -23,6 +21,21 @@ const Home = () => {
     }
   };
 
+  const renderResults = () => {
+    if (results && results.length === 0) {
+      return <div>No results</div>;
+    }
+    if (results && results.length > 0) {
+      return (
+        <div>
+          {results.map(item => (
+            <div key={item.show.id}>{item.show.name}</div>
+          ))}
+        </div>
+      );
+    }
+    return null;
+  };
   return (
     <MainPageLayout>
       <input
@@ -34,6 +47,7 @@ const Home = () => {
       <button type="button" onClick={onSearch}>
         Search
       </button>
+      {renderResults()}
     </MainPageLayout>
   );
 };
