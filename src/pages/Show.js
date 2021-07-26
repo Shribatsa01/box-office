7f2924f681f359c8984877e245b5ae6a6f9f355c
Shribatsa01 <shribatsa01@gmail.com>
2021-07-26 22:38:48 +0530

    created show page components and populated them with data.

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,6 +1,11 @@
+/* eslint-disable no-underscore-dangle */
 import React, { useEffect, useReducer } from 'react';
 import { useParams } from 'react-router-dom';
 import { apiGet } from '../misc/config';
+import ShowMainData from '../components/show/ShowMainData';
+import Details from '../components/show/Details';
+import Seasons from '../components/show/Seasons';
+import Cast from '../components/show/Cast';
 
 const reducer = (prevState, action) => {
   switch (action.type) {
@@ -16,19 +21,24 @@ const reducer = (prevState, action) => {
       return prevState;
   }
 };
+
 const initialState = {
   show: null,
   isLoading: true,
   error: null,
 };
+
 const Show = () => {
   const { id } = useParams();
+
   const [{ show, isLoading, error }, dispatch] = useReducer(
     reducer,
     initialState
   );
+
   useEffect(() => {
     let isMounted = true;
+
     apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`)
       .then(results => {
         if (isMounted) {
@@ -45,14 +55,45 @@ const Show = () => {
       isMounted = false;
     };
   }, [id]);
-  console.log('show', show);
+
   if (isLoading) {
-    return <div>Data is being loaded.</div>;
+    return <div>Data is being loaded</div>;
   }
+
   if (error) {
-    return <div>An error has occured: (error)</div>;
+    return <div>Error occured: {error}</div>;
   }
-  return <div>This is show page</div>;
+
+  return (
+    <div>
+      <ShowMainData
+        image={show.image}
+        name={show.name}
+        rating={show.rating}
+        summary={show.summary}
+        tags={show.genres}
+      />
+
+      <div>
+        <h2>Details</h2>
+        <Details
+          status={show.status}
+          network={show.network}
+          premiered={show.premiered}
+        />
+      </div>
+
+      <div>
+        <h2>Seasons</h2>
+        <Seasons seasons={show._embedded.seasons} />
+      </div>
+
+      <div>
+        <h2>Cast</h2>
+        <Cast cast={show._embedded.cast} />
+      </div>
+    </div>
+  );
 };
 
 export default Show;
