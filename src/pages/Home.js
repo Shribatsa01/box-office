9b39f69641242c29d523508d8ec3a8b4aa8548e8
Shribatsa01 <shribatsa01@gmail.com>
2021-07-27 23:21:38 +0530

    created two more hooks: useLastQuery and useShow.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,15 @@
 import React, { useState } from 'react';
-import ActorGrid from '../components/actor/ActorGrid';
 import MainPageLayout from '../components/MainPageLayout';
-import ShowGrid from '../components/show/ShowGrid';
 import { apiGet } from '../misc/config';
+import ShowGrid from '../components/show/ShowGrid';
+import ActorGrid from '../components/actor/ActorGrid';
+import { useLastQuery } from '../misc/custom-hooks';
 
 const Home = () => {
-  const [input, setInput] = useState('');
+  const [input, setInput] = useLastQuery();
   const [results, setResults] = useState(null);
   const [searchOption, setSearchOption] = useState('shows');
+
   const isShowsSearch = searchOption === 'shows';
   const onSearch = () => {
     apiGet(`/search/${searchOption}?q=${input}`).then(result => {
@@ -33,6 +35,7 @@ const Home = () => {
     if (results && results.length === 0) {
       return <div>No results</div>;
     }
+
     if (results && results.length > 0) {
       return results[0].show ? (
         <ShowGrid data={results} />
@@ -40,8 +43,10 @@ const Home = () => {
         <ActorGrid data={results} />
       );
     }
+
     return null;
   };
+
   return (
     <MainPageLayout>
       <input
@@ -53,16 +58,17 @@ const Home = () => {
       />
 
       <div>
-        <label htmlFor="show-sarch">
+        <label htmlFor="shows-search">
           Shows
           <input
-            id="show-search"
+            id="shows-search"
             type="radio"
             value="shows"
             checked={isShowsSearch}
             onChange={onRadioChange}
           />
         </label>
+
         <label htmlFor="actors-search">
           Actors
           <input
@@ -74,6 +80,7 @@ const Home = () => {
           />
         </label>
       </div>
+
       <button type="button" onClick={onSearch}>
         Search
       </button>
