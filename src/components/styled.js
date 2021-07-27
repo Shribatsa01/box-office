0d520d6fe0a1da67d73ef57b653f28fd50813d13
Shribatsa01 <shribatsa01@gmail.com>
2021-07-27 23:48:15 +0530

    defined theme for styled components and created a few styled components.

--- src/components/styled.js
+++ src/components/styled.js
@@ -38,7 +38,6 @@ export const Star = styled.div`
   display: inline-block;
   width: 18px;
   height: 18px;
-  /* background-color: #ffc806; */
   background-color: ${props => (props.active ? '#ffc806' : '#ddd')};
   clip-path: polygon(
     50% 0%,
